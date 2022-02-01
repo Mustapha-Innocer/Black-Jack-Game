81eea8dc41486304693561887c02de25e52b289a
Mustapha Innocer <mustaphainnocer@gmail.com>
2022-02-01 14:37:05 +0000

    player HIT strategy tested

---
+++ Test/test_strategy.py
@@ -0,0 +1,18 @@
+
+
+from unittest import TestCase
+
+from black_jack import strategy
+from black_jack.Card import Card
+from black_jack.player import Player
+from black_jack.player_status import PlayerStatus
+
+
+class TestStrategy(TestCase):
+    def test_hit(self):
+        card1 = Card("heart", 10)
+        card2 = Card("heart", 2)
+        player = Player("James", card1, card2)
+        strategy.strategy(player)
+        expected_status = PlayerStatus.HIT
+        self.assertEqual(player.status, expected_status)
