10600f3b44a15574a9a269e70d961eb0c04e9e34
jessebrite <aboagye@protonmail.com>
2022-02-01 14:07:07 +0000

    card total added to player class

--- Test/test_player.py
+++ Test/test_player.py
@@ -1,13 +1,19 @@
 from unittest import TestCase
+
+from black_jack.Deck import Deck
 from black_jack.player import Player
 
 
 class TestPlayer(TestCase):
 
     def setUp(self):
-        self.player1 = Player("James")
+        self.deck = Deck()
+        self.player1 = Player("James", self.deck.deal_card(), self.deck.deal_card())
 
     def test_set_status(self):
         self.assertEqual(self.player1.name, "James")
         self.assertEqual(self.player1.status, None)
         self.assertEqual(self.player1.count, 1)
+
+    def test_card_total(self):
+        self.assertEqual(self.player1.total(), 5)
