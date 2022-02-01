4c6792d2b2df1eb0f56681fd191a99632b40effb
Mustapha Innocer <mustaphainnocer@gmail.com>
2022-02-01 15:16:32 +0000

    GO BUST strategy adeded and tested

--- Test/test_strategy.py
+++ Test/test_strategy.py
@@ -10,9 +10,17 @@ from black_jack.player_status import PlayerStatus
 
 class TestStrategy(TestCase):
     def test_hit(self):
-        card1 = Card("heart", 10)
-        card2 = Card("heart", 2)
+        card1 = Card("heart", "10")
+        card2 = Card("heart", "2")
         player = Player("James", card1, card2)
         strategy.strategy(player)
         expected_status = PlayerStatus.HIT
+        self.assertEqual(player.status, expected_status)
+
+    def test_go_bust(self):
+        card1 = Card("heart", "ace")
+        card2 = Card("diamond", "ace")
+        player = Player("James", card1, card2)
+        strategy.strategy(player)
+        expected_status = PlayerStatus.GO_BUST
         self.assertEqual(player.status, expected_status)
